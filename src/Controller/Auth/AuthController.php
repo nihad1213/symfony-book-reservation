b09d8ac5e829d54f807acc9ba226f8367d17c253
nihad1213 <nihad.nemetli@gmail.com>
2025-10-20 23:38:38 +0400

    add: login and register methods added to AuthController

--- src/Controller/Auth/AuthController.php
+++ src/Controller/Auth/AuthController.php
@@ -3,16 +3,29 @@
 namespace App\Controller\Auth;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AuthController extends AbstractController
 {
-    #[Route('/auth', name: 'app_auth')]
-    public function index(): Response
+    #[Route('/auth/login', name: 'app_auth_login')]
+    public function login(Request $request): Response
     {
-        return $this->render('auth/index.html.twig', [
+        // Logic will be writed in here
+
+        return $this->render('auth/login.html.twig', [
             'controller_name' => 'AuthController',
         ]);
     }
+
+    #[Route('/auth/register', name: 'app_auth_register')]
+    public function register(Request $request): Response
+    {
+        // Logic will be writed in here
+        return $this->render('auth/register.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+    
 }
